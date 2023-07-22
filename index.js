779ac4301e3e22d62b8e2d1f1eef6b3275689531
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-23 01:07:21 +0600

    find single college

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
                         res.send(result);
                 })
 
+                app.get('/singleCollege/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: new Object(id) };
+                        const result = await collegeCollection.findOne(query);
+                        res.send(result);
+                })
+
 
 
                 await client.db("admin").command({ ping: 1 });
